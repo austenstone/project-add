9582624801f1147e8be02d0988afca49285dbc80
Austen Stone <austenstone@github.com>
2022-03-08 21:47:26 -0500

    build

--- dist/index.js
+++ dist/index.js
@@ -134,8 +134,11 @@ EX: \u001b[1mhttps://github.com/orgs/github/projects/5380\u001B[m has the number
         core.setFailed(`Failed to add issue to project '${projectNext.title}'.`);
         return;
     }
+    const link = organization ?
+        `https://github.com/orgs/${organization}/projects/${projectNumber}` :
+        `https://github.com/users/${user}/projects/${projectNumber}`;
     core.info(`✅ Successfully added issue \u001b[1m${issue.title}\u001B[m to project \u001b[1m${projectNext.title}\u001B[m.
-https://github.com/orgs/github/projects/${projectNumber}`);
+${link}`);
 });
 exports["default"] = run;
 
