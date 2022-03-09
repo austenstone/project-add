b7573b929a325ad627a0ca23ede479b3cc069564
Austen Stone <austenstone@github.com>
2022-03-08 21:43:38 -0500

    cleanup

--- src/projects.ts
+++ src/projects.ts
@@ -1,26 +1,26 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
+import * as core from '@actions/core'
+import * as github from '@actions/github'
 
-type ClientType = ReturnType<typeof github.getOctokit>;
+type ClientType = ReturnType<typeof github.getOctokit>
 
 const run = async (): Promise<void> => {
-  if (!github.context) return core.setFailed('No GitHub context.');
-  if (!github.context.payload) return core.setFailed('No payload. Make sure this is an issue event.');
-  if (!github.context.payload.issue) return core.setFailed('No issue found in the payload. Make sure this is an issue event.');
-  const token = core.getInput('github-token') || process.env.GITHUB_TOKEN;
-  const projectNumber = parseInt(core.getInput('project_number'));
-  const organization = core.getInput('organization ');
-  const user = core.getInput('user');
-  const issue = github.context.payload.issue;
+  if (!github.context) return core.setFailed('No GitHub context.')
+  if (!github.context.payload) return core.setFailed('No payload. Make sure this is an issue event.')
+  if (!github.context.payload.issue) return core.setFailed('No issue found in the payload. Make sure this is an issue event.')
+  const token = core.getInput('github-token') || process.env.GITHUB_TOKEN
+  const projectNumber = parseInt(core.getInput('project_number'))
+  const organization = core.getInput('organization ')
+  const user = core.getInput('user')
+  const issue = github.context.payload.issue
 
-  if (!token) return core.setFailed('No input \'token\'');
-  if (!projectNumber) return core.setFailed('No input \'projectNumber\'');
-  if (!issue) return core.setFailed('No issue in event context');
+  if (!token) return core.setFailed('No input \'token\'')
+  if (!projectNumber) return core.setFailed('No input \'projectNumber\'')
+  if (!issue) return core.setFailed('No issue in event context')
 
-  const octokit: ClientType = github.getOctokit(token);
+  const octokit: ClientType = github.getOctokit(token)
 
-  core.startGroup(`GraphQL get project number \u001b[1m${projectNumber}\u001B[m`);
-  let projectQuery;
+  core.startGroup(`GraphQL get project number \u001b[1m${projectNumber}\u001B[m`)
+  let projectQuery
   if (organization) {
     projectQuery = `{
       organization(login: "${organization}") {
@@ -54,7 +54,7 @@ const run = async (): Promise<void> => {
   core.info(JSON.stringify(projectNextResponse, null, 2))
   core.endGroup()
 
-  const projectNext = projectNextResponse?.organization?.projectNext || projectNextResponse?.user?.projectNext;
+  const projectNext = projectNextResponse?.organization?.projectNext || projectNextResponse?.user?.projectNext
 
   if (!projectNext?.id) {
     core.setFailed(`Project number \u001b[1m${projectNumber}\u001B[m not found for login \u001b[1m${organization || user}\u001B[m.
@@ -63,7 +63,7 @@ EX: \u001b[1mhttps://github.com/orgs/github/projects/5380\u001B[m has the number
     return
   }
 
-  core.startGroup(`GraphQL add issue \u001b[1m${issue.title}\u001B[m to project \u001b[1m${projectNext.title}\u001B[m`);
+  core.startGroup(`GraphQL add issue \u001b[1m${issue.title}\u001B[m to project \u001b[1m${projectNext.title}\u001B[m`)
   const result: any = await octokit.graphql({
     query: `mutation {
       addProjectNextItem(
@@ -84,8 +84,11 @@ EX: \u001b[1mhttps://github.com/orgs/github/projects/5380\u001B[m has the number
     return
   }
 
+  const link = organization ?
+    `https://github.com/orgs/${organization}/projects/${projectNumber}` :
+    `https://github.com/users/${user}/projects/${projectNumber}`
   core.info(`✅ Successfully added issue \u001b[1m${issue.title}\u001B[m to project \u001b[1m${projectNext.title}\u001B[m.
-https://github.com/orgs/github/projects/${projectNumber}`);
-};
+${link}`)
+}
 
-export default run;
+export default run
