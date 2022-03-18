f18f01f5f0a1a1c4e2384341a9e7c0c821c561d4
Austen Stone <austenstone@github.com>
2022-03-17 23:25:47 -0400

    build

--- dist/index.js
+++ dist/index.js
@@ -70,13 +70,10 @@ function getInputs() {
     else {
         throw `Missing input 'organization' or 'user'`;
     }
-    console.log(ret);
-    console.log(github.context.payload);
-    console.log(github.context.payload.issue);
     if (github.context.payload.issue) {
         ret.issue = github.context.payload.issue;
     }
-    if (github.context.payload.pull_request) {
+    else if (github.context.payload.pull_request) {
         ret.pr = github.context.payload.pull_request;
     }
     else {
