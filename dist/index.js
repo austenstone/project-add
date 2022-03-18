f101114a0d92aeed6c503612c4b81ac6509edd97
Austen Stone <austenstone@github.com>
2022-03-17 23:03:14 -0400

    test

--- dist/index.js
+++ dist/index.js
@@ -50,27 +50,138 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.getInputs = void 0;
 const core = __importStar(__nccwpck_require__(2186));
 const github = __importStar(__nccwpck_require__(5438));
+function getInputs() {
+    const result = {};
+    result.token = core.getInput('github-token');
+    result.projectNumber = parseInt(core.getInput('project-number'));
+    if (isNaN(result.projectNumber))
+        throw `No input 'projectNumber'`;
+    result.organization = core.getInput('organization') || github.context.repo.owner;
+    result.user = core.getInput('user');
+    if (result.organization) {
+        result.login = result.organization;
+    }
+    else if (result.user) {
+        result.login = result.user;
+    }
+    else {
+        throw `Missing payload 'organization' or 'user'`;
+    }
+    if (github.context.payload.issue) {
+        result.issue = github.context.payload.issue;
+    }
+    if (github.context.payload.pull_request) {
+        result.pr = github.context.payload.pull_request;
+    }
+    else {
+        throw `Missing payload 'pull_request' or 'issue'`;
+    }
+    const fields = core.getInput('fields');
+    const fieldsValue = core.getInput('fields-value');
+    const fieldsValueArr = fieldsValue.split(',');
+    if (fields) {
+        result.fields = fields.split(',').reduce((obj, f, i) => {
+            if (fieldsValueArr[i]) {
+                obj[f] = fieldsValueArr[i];
+            }
+            return obj;
+        }, {});
+    }
+    return result;
+}
+exports.getInputs = getInputs;
 const run = () => __awaiter(void 0, void 0, void 0, function* () {
-    var _a, _b, _c, _d;
+    var _a, _b;
     if (!github.context)
         return core.setFailed('No GitHub context.');
     if (!github.context.payload)
         return core.setFailed('No event. Make sure this is an issue or pr event.');
-    const token = core.getInput('github-token') || process.env.GITHUB_TOKEN;
-    const projectNumber = parseInt(core.getInput('project-number'));
-    const organization = core.getInput('organization') || github.context.repo.owner;
-    const user = core.getInput('user');
-    const login = organization || user;
-    const issue = github.context.payload.issue;
-    const pr = github.context.payload.pull_request;
-    if (!token)
-        return core.setFailed('No input \'token\'');
-    if (!projectNumber)
-        return core.setFailed('No input \'projectNumber\'');
-    if (!issue && !pr)
-        return core.setFailed('No issue or pr in event context');
+    const inputs = getInputs();
+    const headers = { 'GraphQL-Features': 'projects_next_graphql', };
+    const projectGet = (projectNumber, organization, user) => __awaiter(void 0, void 0, void 0, function* () {
+        let projectQuery;
+        if (user) {
+            projectQuery = `{
+        user(login: "${user}") {
+          projectNext(number: ${projectNumber}) {
+            title,
+            id
+          }
+        }
+      }`;
+        }
+        else if (organization) {
+            projectQuery = `{
+        organization(login: "${organization}") {
+          projectNext(number: ${projectNumber}) {
+            title,
+            id
+          }
+        }
+      }`;
+        }
+        else {
+            core.setFailed('No input \'organization\' or \'user\'');
+        }
+        const projectNextResponse = yield octokit.graphql(projectQuery);
+        return projectNextResponse;
+    });
+    const projectAdd = (projectId, contentId) => __awaiter(void 0, void 0, void 0, function* () {
+        var _c, _d;
+        const result = yield octokit.graphql({
+            query: `mutation {
+        addProjectNextItem(
+          input: { contentId: "${contentId}", projectId: "${projectId}" }
+        ) {
+          projectNextItem {
+            id
+          }
+        }
+      }`,
+            headers
+        });
+        return (_d = (_c = result === null || result === void 0 ? void 0 : result.addProjectNextItem) === null || _c === void 0 ? void 0 : _c.projectNextItem) === null || _d === void 0 ? void 0 : _d.id;
+    });
+    const projectFieldsGet = (projectId) => __awaiter(void 0, void 0, void 0, function* () {
+        var _e;
+        const result = yield octokit.graphql({
+            query: `{
+        node(id: "${projectId}") {
+          ... on ProjectNext {
+            fields(first: 20) {
+              nodes {
+                id
+                name
+                settings
+              }
+            }
+          }
+        }
+      }`,
+            headers
+        });
+        return (_e = result === null || result === void 0 ? void 0 : result.ProjectNext) === null || _e === void 0 ? void 0 : _e.fields;
+    });
+    const projectFieldUpdate = (projectId, itemId, fieldId, value) => __awaiter(void 0, void 0, void 0, function* () {
+        var _f, _g;
+        const result = yield octokit.graphql({
+            query: `mutation {
+        updateProjectNextItemField(
+          input: {projectId: "${projectId}", itemId: "${itemId}", fieldId: "${fieldId}", value: ${JSON.stringify(value)}}
+        ) {
+          projectNextItem {
+            id
+          }
+        }
+      }`,
+            headers
+        });
+        return (_g = (_f = result === null || result === void 0 ? void 0 : result.updateProjectNextItemField) === null || _f === void 0 ? void 0 : _f.projectNextItem) === null || _g === void 0 ? void 0 : _g.id;
+    });
+    const { issue, pr, projectNumber, token, login, organization, user, fields } = inputs;
     const node_id = (issue === null || issue === void 0 ? void 0 : issue.node_id) || (pr === null || pr === void 0 ? void 0 : pr.node_id);
     if (!node_id)
         return core.setFailed('Can\'t find \'node_id\' in event context');
@@ -78,32 +189,7 @@ const run = () => __awaiter(void 0, void 0, void 0, function* () {
     const title = issue ? issue.title : pr === null || pr === void 0 ? void 0 : pr.title;
     const octokit = github.getOctokit(token);
     core.startGroup(`Get project number \u001b[1m${projectNumber}\u001B[m`);
-    let projectQuery;
-    if (user) {
-        projectQuery = `{
-      user(login: "${user}") {
-        projectNext(number: ${projectNumber}) {
-          title,
-          id
-        }
-      }
-    }`;
-    }
-    else if (organization) {
-        projectQuery = `{
-      organization(login: "${organization}") {
-        projectNext(number: ${projectNumber}) {
-          title,
-          id
-        }
-      }
-    }`;
-    }
-    else {
-        core.setFailed('No input \'organization\' or \'user\'');
-    }
-    const headers = { 'GraphQL-Features': 'projects_next_graphql', };
-    const projectNextResponse = yield octokit.graphql(projectQuery);
+    const projectNextResponse = yield projectGet(projectNumber, organization, user);
     core.info(JSON.stringify(projectNextResponse, null, 2));
     core.endGroup();
     const projectNext = ((_a = projectNextResponse === null || projectNextResponse === void 0 ? void 0 : projectNextResponse.organization) === null || _a === void 0 ? void 0 : _a.projectNext) || ((_b = projectNextResponse === null || projectNextResponse === void 0 ? void 0 : projectNextResponse.user) === null || _b === void 0 ? void 0 : _b.projectNext);
@@ -114,23 +200,22 @@ EX: \u001b[1mhttps://github.com/orgs/github/projects/1234\u001B[m has the number
         return;
     }
     core.startGroup(`Add ${type} \u001b[1m${title}\u001B[m to project \u001b[1m${projectNext.title}\u001B[m`);
-    const result = yield octokit.graphql({
-        query: `mutation {
-      addProjectNextItem(
-        input: { contentId: "${node_id}", projectId: "${projectNext.id}" }
-      ) {
-        projectNextItem {
-          id
-        }
-      }
-    }`,
-        headers
-    });
-    core.info(JSON.stringify(result, null, 2));
+    const itemId = yield projectAdd(projectNext.id, node_id);
+    core.info(JSON.stringify(itemId, null, 2));
     core.endGroup();
-    if (!((_d = (_c = result === null || result === void 0 ? void 0 : result.addProjectNextItem) === null || _c === void 0 ? void 0 : _c.projectNextItem) === null || _d === void 0 ? void 0 : _d.id)) {
+    if (itemId) {
         core.setFailed(`Failed to add ${type} to project '${projectNext.title}'.`);
         return;
+    }
+    if (fields) {
+        const projectFields = yield projectFieldsGet(projectNext.id);
+        console.log('fields', fields);
+        console.log('projectFields', projectFields);
+        Object.entries(fields).forEach(([key, value]) => {
+            const fieldId = projectFields[key];
+            const updatedFieldId = projectFieldUpdate(projectNext.id, itemId, fieldId, value);
+            core.info(JSON.stringify(updatedFieldId, null, 2));
+        });
     }
     const link = `https://github.com/${user ? 'users/' + user : 'orgs/' + organization}/projects/${projectNumber}`;
     core.info(`✅ Successfully added ${type} \u001b[1m${title}\u001B[m to project \u001b[1m${projectNext.title}\u001B[m.
